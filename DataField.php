a743ef7c4cf9a03f6ff1642e7aed7df854beee68
Toth-Gabor <upcgabor@gmail.com>
2019-11-13 14:32:35 +0100

    Added getters

--- DataField.php
+++ DataField.php
@@ -12,10 +12,34 @@ class DataField extends BaseField
         parent::__construct($xCoord, $yCoord);
     }
 
+    /**
+     * @return int
+     */
+    public function getXCoord(): int
+    {
+        return $this->xCoord;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYCoord(): int
+    {
+        return $this->yCoord;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
     public function __toString()
     {
         return 'Value: ' . $this->value .
-            PHP_EOL. 'X coordinate: ' . parent::getXCoord() .
+            PHP_EOL . 'X coordinate: ' . parent::getXCoord() .
             PHP_EOL . 'Y coordinate: ' . parent::getYCoord();
     }
 }
