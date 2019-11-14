ae309050c8184724bcc09959bffa8680d4771797
Toth-Gabor <upcgabor@gmail.com>
2019-11-14 12:54:22 +0100

    Refactored message

--- exceptions/InvalidCoordinatesException.php
+++ exceptions/InvalidCoordinatesException.php
@@ -5,7 +5,7 @@ class InvalidCoordinatesException extends Exception
 {
     public function __construct($code = 0, Throwable $previous = null)
     {
-        parent::__construct("The given coordinates are not valid!", $code, $previous);
+        parent::__construct(PHP_EOL . "The given coordinates are not valid!", $code, $previous);
     }
 
 }
--- exceptions/NoValueOnThisCoordinatesException.php
+++ exceptions/NoValueOnThisCoordinatesException.php
@@ -5,7 +5,7 @@ class NoValueOnThisCoordinatesException extends Exception
 {
     public function __construct($code = 0, Throwable $previous = null)
     {
-        parent::__construct("There is'nt any data on the given coordinates!", $code, $previous);
+        parent::__construct(PHP_EOL . "There is'nt any data on the given coordinates!", $code, $previous);
     }
 
 }
