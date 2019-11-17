bda7d3760e327c001172783465548228fe2704a6
Toth-Gabor <upcgabor@gmail.com>
2019-11-17 13:21:48 +0100

    Added XmlService class

---
+++ models/fileHandling/XmlService.php
@@ -0,0 +1,45 @@
+<?php
+
+
+class XmlService implements DataService
+{
+    public function read()
+    {
+        // TODO: Implement read() method.
+    }
+
+    /**
+     * @param string $name
+     * @param TableDoc $tableDoc
+     */
+    public function write(string $name, TableDoc $tableDoc)
+    {
+        $xmlDoc = $this->convert($tableDoc);
+        //save xml file
+        $file_name = str_replace(' ', '_', $name) . '_' . time() . '.xml';
+        $xmlDoc->save($file_name);
+    }
+
+    /**
+     * @param TableDoc $doc
+     * @return DOMDocument
+     */
+    private function convert(TableDoc $doc): DOMDocument
+    {
+        $xmlDoc = new DOMDocument();
+
+        $root = $xmlDoc->appendChild($xmlDoc->createElement($doc->getName()));
+        $tables = $root->appendChild($xmlDoc->createElement('tables'));
+        foreach ($doc->getDataTables() as $dataTable) {
+            $table = $tables->appendChild($xmlDoc->createElement('table', $dataTable->getName()));
+            foreach ($dataTable->getDataFields() as $dataField) {
+                $fields = $table->appendChild($xmlDoc->createElement('fields'));
+                $fields->appendChild($xmlDoc->createElement('xCoord', $dataField->getXCoord()));
+                $fields->appendChild($xmlDoc->createElement('yCoord', $dataField->getYCoord()));
+                $fields->appendChild($xmlDoc->createElement('value', $dataField->getValue()));
+            }
+        }
+        $xmlDoc->formatOutput = true;
+        return $xmlDoc;
+    }
+}
