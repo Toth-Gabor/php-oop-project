7f3fc6da2ad394889bca91644f266e9515bb0cf9
Toth-Gabor <upcgabor@gmail.com>
2019-11-17 15:45:24 +0100

    Added IntegerField class

---
+++ models/fields/IntegerField.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class IntegerField extends Field
+{
+    /**
+     * @var int
+     */
+    protected $value;
+
+    /**
+     * IntegerField constructor.
+     * @param int $xCoord
+     * @param int $yCoord
+     * @param int $value
+     */
+    public function __construct(int $xCoord, int $yCoord, int $value)
+    {
+        parent::__construct($xCoord, $yCoord, $value);
+    }
+}
