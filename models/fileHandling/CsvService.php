0c64bde14d45b817e851fafdaf7c34880c1b7831
Toth-Gabor <upcgabor@gmail.com>
2019-11-17 13:21:11 +0100

    Added CsvService class

---
+++ models/fileHandling/CsvService.php
@@ -0,0 +1,40 @@
+<?php
+
+
+class CsvService implements DataService
+{
+    public function read()
+    {
+        // TODO: Implement read() method.
+    }
+
+    /**
+     * @param string $name
+     * @param TableDoc $tableDoc
+     */
+    public function write(string $name, TableDoc $tableDoc)
+    {
+        $data = $this->convert($tableDoc);
+        $fp = fopen($name . '.csv', 'w+');
+
+        foreach ($data as $fields) {
+            fputcsv($fp, $fields);
+        }
+
+        fclose($fp);
+    }
+
+    private function convert(TableDoc $doc): array
+    {
+        $data = array();
+        $data[] = array('tableName', 'xCoord', 'yCoord', 'value');
+        $tableList = $doc->getDataTables();
+        foreach ($tableList as $table){
+            $fields = $table->getDatafields();
+            foreach ($fields as $dataField){
+                $data[] = array($table->getName(), $dataField->getXCoord(), $dataField->getYCoord(), $dataField->getValue());
+            }
+        }
+        return $data;
+    }
+}
